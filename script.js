f02315eb6172ffa9d681f0b6e5251de21fa7e80a
Younghyun Lee <younghyunlee22@gmail.com>
2023-12-03 21:55:52 -0600

    sum for each

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+write functions that accomplish the following:
+Take an array of numbers and return the sum.
+Take an array of numbers and return the average.
+Take an array of strings and return the longest string.
+Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+Take a number, n, and print every number between 1 and n without using loops. Use recursion. */
+
+function sumOfNumArray(arr) {
+  let sum = 0;
+  arr.forEach(function (num) {
+    sum += num;
+  });
+  return sum;
+}
+
+console.log(sumOfNumArray([1, 2, 3, 4])); // 10
